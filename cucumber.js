efa70a4e58f5ba2cc95d5a889af34067f6092a60
sankar-c-hub <134898758+sankar-c-hub@users.noreply.github.com>
2026-02-15 15:27:52 +0530

    Add parallel execution support in cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -1,4 +1,7 @@
+const JsonUtility = require('./utils/json_utility.js');
 const timestamp = new Date().toISOString().replace(/:/g, '-').split('.')[0];
+
+const isParallelEnabled = JsonUtility.getConfigValue('parallel') === true;
 
 module.exports = {
   default: {
@@ -7,11 +10,16 @@ module.exports = {
       './common/**/*.js'
     ],
     paths: ['features/**/*.feature'],
+
+    // 👇 Conditional Parallel
+    ...(isParallelEnabled && { parallel: 5 }),
+
     format: [
       'progress',
       `json:reports/${timestamp}/cucumber-report.json`,
       `html:reports/${timestamp}/cucumber-report.html`
     ],
+
     publishQuiet: true
   }
 };
